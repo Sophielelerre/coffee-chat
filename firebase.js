b29a162d20e939aef7765c57ee29d6398dd6f55c
Sophielelerre <anne.lelerre@gmail.com>
2025-05-19 21:46:21 +0200

    Firebase addition Using the Modular SDK with <script type="module"> directly in index.html

---
+++ firebase.js
@@ -0,0 +1,19 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAteHauQYee_KrazD8aUFZ5MIUFFw6I4yM",
+  authDomain: "coffee-chat-a1171.firebaseapp.com",
+  projectId: "coffee-chat-a1171",
+  storageBucket: "coffee-chat-a1171.firebasestorage.app",
+  messagingSenderId: "46868898105",
+  appId: "1:46868898105:web:ebd313d03cae07f21a88e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
